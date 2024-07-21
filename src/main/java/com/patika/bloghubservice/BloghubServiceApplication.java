0640f36a24ef6feac4a97454132ea09283b84248
ucarrr <ucaaar07@gmail.com>
2024-07-21 12:44:43 +0300

    initial commit

---
+++ src/main/java/com/patika/bloghubservice/BloghubServiceApplication.java
@@ -0,0 +1,13 @@
+package com.patika.bloghubservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloghubServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloghubServiceApplication.class, args);
+    }
+
+}
